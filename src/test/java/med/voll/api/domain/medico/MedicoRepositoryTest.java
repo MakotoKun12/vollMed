1bd6c87e74f32bc01a08e0ce3b74a701a7b38b0d
Juan <jmha0604@hotmail.com>
2024-07-04 09:00:45 -0600

    third commit configuration test

--- src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
+++ src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -34,6 +34,7 @@ class MedicoRepositoryTest {
     @DisplayName("Deberia retornar nulo cuando el medico se encuentre en consulta con otro paciente en ese horario")
     void seleccionarMedicoConEspecialidadEnFechaEscenario1() {
 
+        //given
         var proximoLunes10H = LocalDate.now()
                 .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
                 .atTime(10,0);
@@ -42,9 +43,10 @@ class MedicoRepositoryTest {
         var paciente = registrarPaciente("Antonio","a@mail.com","645890");
         registrarConsulta(medico,paciente,proximoLunes10H);
 
-
+        //when
         var medicoLibre = medicoRepository.seleccionarMedicoConEspecialidadEnFecha(Especialidad.CARDIOLOGIA,proximoLunes10H);
 
+        //then
         assertThat(medicoLibre).isNull();
     }
 
@@ -52,17 +54,19 @@ class MedicoRepositoryTest {
     @DisplayName("Deberia retornar un medico cuando realice la consulta en la base de datos para ese horario")
     void seleccionarMedicoConEspecialidadEnFechaEscenario2() {
 
+        //given
         var proximoLunes10H = LocalDate.now()
                 .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
                 .atTime(10,0);
 
         var medico = registrarMedico("Jose","j@mail.com","123456",Especialidad.CARDIOLOGIA);
 
+        //when
         var medicoLibre = medicoRepository.seleccionarMedicoConEspecialidadEnFecha(Especialidad.CARDIOLOGIA,proximoLunes10H);
 
+        //then
         assertThat(medicoLibre).isEqualTo(medico);
     }
-
 
     private void registrarConsulta(Medico medico, Paciente paciente, LocalDateTime fecha) {
         em.persist(new Consulta(null, medico, paciente, fecha, null));
